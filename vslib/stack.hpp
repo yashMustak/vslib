253923f92bfe3f5ba9572ae7ff6b34757ff6a0ba
Yash Srivastava <srivastavayash1234@gmail.com>
2019-04-25 22:45:24 +0530

    Update stack.hpp

--- vslib/stack.hpp
+++ vslib/stack.hpp
@@ -7,7 +7,7 @@ using namespace std;
 
 namespace vip {
 	template<class T>
-	class stack {
+	class Stack {
 		private:
 			DoublyLinkedList<T *> mainStack;
 			DoublyLinkedList<T> dataHolder;
@@ -22,41 +22,43 @@ namespace vip {
 }
 
 template<class T>
- bool vip::stack<T>::isEmpty(){
+ bool vip::Stack<T>::isEmpty(){
 	if(top == -1)return true;
 	else return false;
 }
 
 template<class T>
-vip::stack<T>::stack() {
+vip::Stack<T>::Stack() {
 	top = -1;
 }
 
 template<class T>
-void vip::stack<T>::push(T value) {
+void vip::Stack<T>::push(T value) {
 	dataHolder.insert(value);
 	mainStack.insert(dataHolder.lastDataReference());
 	top++;
 }
 
 template<class T>
-void vip::stack<T>::push(T * value) {
+void vip::Stack<T>::push(T * value) {
 	mainStack.insert(value);
 	top++;
 }
 
 
-/*The pop function removes the last member of dataHolder list
-* this is a problem since if the data is directly addressed by the user
-* and not in the dataHolder list, then we will lose the data from dataHolder
-* instead of this just try to check weather if the data, which is refferenced
+/*
+* The pop function removes the last member of dataHolder list
+* this can be a problem because if the data is directly addressed by the user
+* and not in the dataHolder list, then the data will be lost from dataHolder
+* instead of this, just try to check weather if the data which is refferenced
 * in main stack is from dataHolder or directly addressed by the user.
-* To check, we must add some functionality in linkedlist file 
-* which could give access of looping in list or just tell me
-* weather this refference(the refference which is poped) is of any member in it. */
+* To check this we must add some functionality in linkedlist file 
+* which could give access of looping in list or just get the information
+* weather this refference (the refference which is poped) is of any member in it? 
+*/
 
 template<class T>
-T vip::stack<T>::pop() {
+T vip::Stack<T>::pop() {
 	if(top>=0){
 		T * poped = mainStack[top];
 		T popedData = *poped;
